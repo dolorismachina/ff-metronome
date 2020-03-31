55414d13b048788427c3f46eb530e00525838e33
Radoslaw Polczyk <dolorismachina@gmail.com>
2020-04-01 00:47:46 +0100

    Extract message handling logic into functions

--- background/background.js
+++ background/background.js
@@ -4,40 +4,69 @@ import App from './app.js'
 let desiredTempo = 120
 let app = null
 
-// Listen to messages from popup
-browser.runtime.onMessage.addListener((message, sender, respond) => {
-  if (message.action === 'status') {
-    if (!app) {
-      respond({
-        status: 'offline'
-      })
-    }
-    else respond({
-      status: app.status,
-      bpm: app.bpm
+
+function handleMessageStatus(message, sender, respond) {
+  if (!app) {
+    respond({
+      status: 'offline'
     })
+  }
+  else respond({
+    status: app.on,
+    bpm: app.bpm
+  })
+}
+
+
+function handleMessageTempo(message, sender, respond) {
+  desiredTempo = parseInt(message.value)
+
+  if (app) {
+    app.adjustTempo(message.value)
+  }
+}
+
+
+function handleMessageToggle(message, sender, respond) {
+  if (!app) {
+    app = new App()
+    app.adjustTempo(desiredTempo)
+  }
+
+  else if (app.on) {
+    app.on = false
+    app.adjustGain(0)
+    app.context.close()
+    app = null
+  } 
+}
+
+
+function handleMessage({message, sender, respond}, handler) {
+  handler(message, sender, respond)
+}
+
+
+function onMessageHandler(message, sender, respond) {
+  const data = {
+    message,
+    sender,
+    respond
+  }
+
+  if (message.action === 'status') {
+    handleMessage(data, handleMessageStatus)
   } 
 
   else if (message.action === 'tempo') {
-    if (app) {
-      app.adjustTempo(message.value)
-      desiredTempo = message.value
-    }
-    else desiredTempo = message.value
+    handleMessage(data, handleMessageTempo)
   } 
 
   else if (message.action === 'toggle') {
-    if (!app) {
-      console.log('new')
-      app = new App()
-      app.adjustTempo(desiredTempo)
-    }
-
-    else if (app.on) {
-      app.on = false
-      app.adjustGain(0)
-      app.context.close()
-      app = null
-    }
+    handleMessage(data, handleMessageToggle)
   } 
-})
+}
+
+
+// Listen to messages from popup
+browser.runtime.onMessage.addListener(onMessageHandler)
